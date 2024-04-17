e45046fa2593123c2f634c5008cd6647aea55e98
areski <areski@gmail.com>
2024-04-17 18:45:10 +0200

    [fix](general) fix injection

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,9 @@ import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [react({
+    include: '**/*.disabled',
+  }), svgr()],
   root: resolve('./'),
   base: '/static/',
   server: {
